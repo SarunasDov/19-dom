02ea922735d64c8f2b8bcbf31798e4997ad3303d
Sarunas <sar.dovydaitis@hotmail.com>
2021-06-04 20:51:45 +0300

    to the heaven

--- js/main.js
+++ js/main.js
@@ -1,25 +1,10 @@
-let rezultatas = 0;
-const gryzta = 'Te prasideda zaidynes';
+const buttonDOM = document.querySelector('button');
+const inputDOM = document.querySelector('input');
+const spanDOM = document.querySelector('span');
 
-const minusDOM = document.querySelector('.minus');
-const plusDOM = document.querySelector('.plus');
-const numberDOM = document.querySelector('.number');
-const resetDOM = document.querySelector('.reset');
-const antrasteDOM = document.querySelector('h1')
+buttonDOM.addEventListener('click', (event) => {
+    event.preventDefault();
+    spanDOM.innerText = inputDOM.value;
+    inputDOM.value = '';
 
-minusDOM.addEventListener('click', () => {
-    numberDOM.innerText = --rezultatas;
-    antrasteDOM.innerText = 'Zaidimas prasideda';
-
-})
-
-plusDOM.addEventListener('click', () => {
-    numberDOM.innerText = ++rezultatas;
-    antrasteDOM.innerText = 'Zaidimas prasideda';
-})
-
-resetDOM.addEventListener('click', () => {
-    rezultatas = 0;
-    numberDOM.innerText = rezultatas;
-    antrasteDOM.innerText = gryzta;
 })
