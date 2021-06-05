1ebaa53f9191770aef7faca043d4abcd00f4989b
Sarunas <sar.dovydaitis@hotmail.com>
2021-06-05 13:54:30 +0300

    nedamustas

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
+
 const buttonDOM = document.querySelector('button');
-const inputDOM = document.querySelector('input');
-const spanDOM = document.querySelector('span');
+const orderDOM = document.querySelector(.order);
+const soupDOM = document.querySelector('#sriuba');
+const customerDOM = document.querySelector('#customer');
+const desert = document.querySelector('#desert');
+const drinkDOM = document.querySelector('#drink');
+const soupDOM = document.querySelector('#sriuba');
 
 buttonDOM.addEventListener('click', (event) => {
     event.preventDefault();
-    spanDOM.innerText = inputDOM.value;
-    inputDOM.value = '';
+    function menuCheked(item) {
+        return 'nori';
+    }
+    return 'nenori';
+});
 
-})
+for
