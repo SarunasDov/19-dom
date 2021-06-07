f694666da6bc142ab6f7392183a95fde4efce88f
Sarunas <sar.dovydaitis@hotmail.com>
2021-06-07 21:15:08 +0300

    12

--- js/main.js
+++ js/main.js
@@ -1,35 +1,49 @@
-const klientasDOM = document.querySelector('#customer');
+const submitDOM = document.querySelector('button');
+const orderDOM = document.querySelector('.order');
+const nameDOM = document.querySelector('#customer');
 const sriubaDOM = document.querySelector('#sriuba');
-const pgrPatiekalasDOM = document.querySelector('#patiekalas');
+const pagrindinisPatiekalasDOM = document.querySelector('#patiekalas');
 const desertasDOM = document.querySelector('#desertas');
-const gerimaiDOM = document.querySelectorAll('.drinks');
+const allGerimaiDOM = document.querySelectorAll('input[name="gerimas"]');
 
-const uzsakytiDOM = document.querySelector('button');
-const uzsakymasDOM = document.querySelector('.order');
+function arNoriu(DOM) {
+    return DOM.checked ? 'nori' : 'nenori';
+}
 
-uzsakytiDOM.addEventListener('click', (event) => {
-    event.preventDefault();
-
-    function pasirinktiMaista(item) {
-        if (item.checked) {
-            return 'nori';
-        }
-        return 'nenori';
-    }
-
-    function pasirinktiGerima(item) {
-        let gerimas = '';
-        for (let i = 0; i < item.length; i++) {
-            if (item[i].checked === true) {
-                return (gerimas = item[i].value);
-            }
+function kurisPazymetas(DOMlist) {
+    for (const itemDOM of DOMlist) {
+        if (itemDOM.checked) {
+            return itemDOM.value;
         }
     }
+}
 
-    uzsakymasDOM.innerText = `Klientas ${klientasDOM.value
-        }, ${pasirinktiMaista(sriubaDOM)} sriubos, ${pasirinktiMaista(
-            pgrPatiekalasDOM
-        )} pagrindinio patiekalo, ${pasirinktiMaista(
-            desertasDOM
-        )} deserto ir ${pasirinktiGerima(gerimaiDOM)} yra pasirinktas gerimas.`;
-});
+submitDOM.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const vardas = nameDOM.value.trim();
+    const pasirinktasGerimas = kurisPazymetas(allGerimaiDOM);
+    const sakinioPradzia = vardas ? 'vardu ' + vardas : 'nenudores vardo';
+    const sakinioPabaiga = pasirinktasGerimas ? pasirinktasGerimas + ' yra pasirinktas gerimas' : 'gerimo nepasirinko';
+
+    orderDOM.innerText = `Uzsakovas ${sakinioPradzia} ${arNoriu(sriubaDOM)} sriubos, ${arNoriu(pagrindinisPatiekalasDOM)} pagrindinio patiekalo, ${arNoriu(desertasDOM)} deserto ir ${sakinioPabaiga}.`;
+})
+
+/*
+PRADZIA:
+Uzsakovas vardu X nori....
+Uzsakovas nenurodes vardo nori....
+
+PABAIGA:
+ir X yra pasirinktas gerimas.
+ir gerimo nepasirinko.
+
+
+EXTRA:
+Uzsakovas vardu X nori sriubos, pagrindinio patiekalo, deserto ir [gerimas]
+Uzsakovas vardu X nori sriubos, pagrindinio patiekalo, bet nenori deserto ir [gerimas]
+Uzsakovas vardu X nori sriubos, bet nenori pagrindinio patiekalo, deserto ir [gerimas]
+Uzsakovas vardu X nenori sriubos, pagrindinio patiekalo, deserto ir [gerimas]
+
+
+*/
