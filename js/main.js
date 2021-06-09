0bb73ae0ee98efb057a5e0eb5d1cc304b44e5389
Sarunas <sar.dovydaitis@hotmail.com>
2021-06-09 20:43:00 +0300

    beveik geras

--- js/main.js
+++ js/main.js
@@ -1,63 +1,59 @@
-/*
-const lentaDOM = document.querySelector('.lenta');
-const namuRezDOM = lentaDOM.querySelector('[data-komanda="namu"]');
-const sveciuRezDOM = lentaDOM.querySelector('[data-komanda="sveciu"]');
 
-const aiksteleDOM = document.querySelector('.aikstele');
-const namuKomandaDOM = aiksteleDOM.querySelector('[data-komanda="namu"]');
-const taskaiNamuDOM = namuKomandaDOM.querySelectorAll('button');
+// function generuotiSocials(selector, array) {
+//     const iconsDOM = document.querySelector(selector);
+//     if (!Array.isArray(array) ||
+//         array.length === 0) {
+//         console.error('ERROR: nerastas elementas');
+//         return false;
+//     }
 
-const sveciuKomandaDOM = aiksteleDOM.querySelector('[data-komanda="sveciu"]');
-const taskaiSveciuDOM = sveciuKomandaDOM.querySelectorAll('button');
-
-
-let namuKomandosTaskai = 0;
-
-for (let i = 0; i < taskaiNamuDOM.length; i++) {
-    const mygtukas = taskaiNamuDOM[i];
-    mygtukas.addEventListener('click', () => {
-        namuKomandosTaskai += i + 1;
-        namuRezDOM.innerText = namuKomandosTaskai;
-    })
-}
-
-let sveciuKomandosTaskai = 0;
-
-for (let i = 0; i < taskaiSveciuDOM.length; i++) {
-    const mygtukas = taskaiSveciuDOM[i];
-    mygtukas.addEventListener('click', () => {
-        sveciuKomandosTaskai += i + 1;
-        sveciuRezDOM.innerText = sveciuKomandosTaskai;
-    })
-}
-
-*/
-
-// const lentaDOM = document.querySelector('.lenta');
-// const divDOM = document.querySelector('.div');
+//     let HTML = '';
+//     for (let element of array) {
+//         HTML += `<i class="fa fa-${element}"></i>`;
+//     }
+//     iconsDOM.innerHTML = HTML;
+// }
 
 
-function generuotiLenta(selector, count) {
-    const DOM = document.querySelector(selector);
-    if (!DOM) {
+// const icons = ['facebook', 'twitter', 'linkedin'];
+// const icons1 = [];
+// generuotiSocials('.socials', icons1);
+
+
+
+
+
+
+function generuotiSocials(selector, icons) {
+
+    if (selector === '' ||
+        typeof selector !== 'string') {
         console.error('ERROR: nerastas elementas');
         return false;
     }
 
-    if (count < 0 ||
-        count % 1 !== 0) {
-        console.error('ERROR: skaicius turi buti teigiamas');
+    if (!Array.isArray(icons) ||
+        icons.length === 0) {
+        console.error('ERROR: negali buti tuscias');
+        return false;
+    }
+
+    const iconsDOM = document.querySelector(selector);
+    if (!iconsDOM) {
+        console.error('ERROR: nepavyko rasti elemento');
         return false;
     }
 
     let HTML = '';
-    for (let i = 1; i <= count; i++) {
-        HTML += `<div>${i}</div>`;
-
+    for (let i = 0; i < icons.length; i++) {
+        if (typeof icons[i] === 'string' &&
+            icons[i] !== '') {
+            HTML += `<i class="fa fa-${icons[i]}"></i>`;
+        }
     }
 
-    DOM.innerHTML = HTML;
+    iconsDOM.innerHTML = HTML;
 
 }
-
-generuotiLenta('.lenta', 4);
+const icons = ['facebook', 5, [], 'twitter', 'linkedin'];
+generuotiSocials('.socials', icons);
