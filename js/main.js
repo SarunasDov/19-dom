908bbeb23e564a71e42ce3d2ac13a0e0e1ffe608
Sarunas <sar.dovydaitis@hotmail.com>
2021-06-14 20:48:04 +0300

    18 daryta

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 // Pirmas variantas
 
 // const lentosdydisDOM = document.querySelector('.option');
@@ -44,26 +45,44 @@
 //     DOM.insertAdjacentHTML('beforeend', rowHTML);
 // }
 
+*/
 
-const button = document.querySelector('button');
-const input = document.querySelector('#board');
-const option = document.querySelector('.option');
 
-const board = document.querySelector('.board');
+const optionDOM = document.querySelector('.option');
+const buttonDOM = document.querySelector('button');
+const inputDOM = document.getElementById('board');
+const boardDOM = document.querySelector('.board');
 
-button.addEventListener('click', (e) => {
+buttonDOM.addEventListener('click', (e) => {
     e.preventDefault();
-    // const n = +input.value;  <----------- blogesnis variantas
-    const n = parseInt(input.value);
-    option.textContent = `${n}x${n}`;
-    renderChessBoard(n, board);
-});
+    const n = parseInt(inputDOM.value);
+    optionDOM.textContent = `${n}x${n}`;
+    renderBoard(boardDOM, n);
+})
 
-function renderChessBoard(boardSize, DOM) {
-    let rowHTML = '';
-    const rowSize = 100 / boardSize;
-    for (let i = 1; i <= boardSize; i++) {
-        rowHTML += `<div class="row" style="height:${rowSize}%;"></div>`
+function renderBoard(DOMelement, size) {
+    const elementSize = 100 / size;
+    let whiteBlackCellHTML = '';
+    let blackWhiteCellHTML = '';
+    let HTML = '';
+    const whiteCellHTML = `<div class="cell" style="width: ${elementSize}%; background-color: white;"></div>`;
+    const blackCellHTML = `<div class="cell" style="width: ${elementSize}%; background-color: black;"></div>`;
+
+    for (let c = 0; c < size; c++) {
+        if (c % 2 === 0) {
+            whiteBlackCellHTML += whiteCellHTML;
+            blackWhiteCellHTML += blackCellHTML;
+        } else {
+            whiteBlackCellHTML += blackCellHTML;
+            blackWhiteCellHTML += whiteCellHTML;
+        }
     }
-    DOM.innerHTML = rowHTML;
+
+    let cellHTML = '';
+    for (let r = 0; r < size; r++) {
+        cellHTML = r % 2 === 0 ? whiteBlackCellHTML : blackWhiteCellHTML;
+        HTML += `<div class="row" style="height: ${elementSize}%;">${cellHTML}</div>`;
+    }
+
+    DOMelement.innerHTML = HTML;
 }
